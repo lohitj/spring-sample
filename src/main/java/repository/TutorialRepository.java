20e8c40515b45203e982c706ddf104d61f29ec1e
helpFolks <helpFolks@gmail.com>
2020-04-05 12:29:38 +0530

    First Copmmit

---
+++ src/main/java/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package repository;
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import model.Tutorial;
+
+	public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+	  List<Tutorial> findByTitleContaining(String title);
+	  List<Tutorial> findByPublished(boolean published);
+	}
+
